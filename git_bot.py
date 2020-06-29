f4c50af6de45e63c7cc68618bfb8bc9084b68ff7
Fajrin <sendaljpt24@gmail.com>
2020-06-30 00:35:51 +0700

    add update tags and refs

--- git_bot.py
+++ git_bot.py
@@ -1,4 +1,5 @@
-# /tag --repo=inirepo --commit=hashcom --tag=initagnya
+# /tag --repo=slack-bot --commit=78bb29d32bd3142307649b40ed0774934be3cc9c --tag=staging
+# /tag --repo=slack-bot --commit=f62fd5c52871ba375eb4751b9f2947abe553ebcd --tag=development
 
 import os
 
@@ -108,23 +109,61 @@ async def tag_git(request):
             # post_tags = requests.post(url_tag, data = json_dumps(data_tags), auth = (USER_GIT, PSWD_GIT))
 
             # MAKE RELEASE TAGS
-            # POST /repos/:owner/:repo/releases
+            if __repo != None and __commit_hash != None and __tag_name != None:
 
-            url_release = '{}/repos/{}/{}/releases'.format(URL_GIT, USER_GIT, __repo)
-            data_release = {
-                "tag_name": __tag_name,
-                "target_commitish": __commit_hash,
-                "name": __tag_name,
-                "body": "Release to {}".format(__tag_name),
-                "draft": False,
-                "prerelease": False,
-            }
+                # check tag if exist
+                # GET /repos/:owner/:repo/releases/tags/:tag
+                url_check_tag = '{}/repos/{}/{}/releases/tags/{}'.format(URL_GIT, USER_GIT, __repo, __tag_name)
+                check_tag = requests.get(url_check_tag, auth=HTTPBasicAuth(USER_GIT, PSWD_GIT))
+                # if exist update tag
+                if check_tag.status_code == 200:
+                    existing_tag_release = check_tag.json()
+                    # do update
+                    # PATCH /repos/:owner/:repo/releases/:release_id
+                    url_update_tag = '{}/repos/{}/{}/releases/{}'.format(URL_GIT, USER_GIT, __repo, existing_tag_release.get('id'))
 
-            post_release = requests.post(url_release, data = json_dumps(data_release), auth = (USER_GIT, PSWD_GIT))
+                    data_release = {
+                        "tag_name": __tag_name,
+                        "target_commitish": __commit_hash,
+                        "name": __tag_name,
+                        "body": "Update Release to {}".format(__tag_name),
+                        "draft": False,
+                        "prerelease": False,
+                    }
 
-            await send_to_slack(channel=TO_CHANNEL, text='Hallo {}'.format(user_profile.get('email')))
+                    patch_release = requests.patch(url_update_tag, data = json_dumps(data_release), auth = (USER_GIT, PSWD_GIT))
 
-            return json({'message': 'Done!'})
+                    # after update tag release we must update refs commit of tag
+                    # POST /repos/:owner/:repo/git/refs
+                    url_ref = "{}/repos/{}/{}/git/refs/tags/{}".format(URL_GIT, 'sendaljpt', 'slack-bot', __tag_name)
+                    data_ref = {
+                        "sha": __commit_hash,
+                        "force": True
+                    }
+                    patch_ref = requests.patch(url_ref, data = json_dumps(data_ref), auth = (USER_GIT, PSWD_GIT))
+                    print(patch_release)
+
+                
+                # if not exist create new tag
+                # POST /repos/:owner/:repo/releases
+                elif check_tag.status_code == 404:
+                    url_release = '{}/repos/{}/{}/releases'.format(URL_GIT, USER_GIT, __repo)
+                    data_release = {
+                        "tag_name": __tag_name,
+                        "target_commitish": __commit_hash,
+                        "name": __tag_name,
+                        "body": "Release to {}".format(__tag_name),
+                        "draft": False,
+                        "prerelease": False,
+                    }
+
+                    post_release = requests.post(url_release, data = json_dumps(data_release), auth = (USER_GIT, PSWD_GIT))
+
+                await send_to_slack(channel=TO_CHANNEL, text='Hallo {}'.format(user_profile.get('email')))
+
+                return json({'message': 'Done!'})
+
+            return json({'message': 'Invalid syntax'})
 
         except SlackApiError as e:
 
